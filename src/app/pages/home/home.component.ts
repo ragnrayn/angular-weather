70d9c93dade1a59f26f5db220b3339cfdca3621a
ragnrayn <ragnrayn@gmail.com>
2024-11-08 21:35:41 +0200

    added search weather by country name - basic functionality

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,23 +2,33 @@ import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { FooterComponent } from '../../components/footer/footer.component';
 import { WeatherService } from '../../services/weather.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent implements OnInit {
 
   weatherData: any = {};
+  searchText: string = "";
 
   constructor(private weatherService: WeatherService){}
   
   ngOnInit(): void {  
-    this.weatherService.getCurrentWeather().subscribe(data => {
+    this.weatherService.getCurrentWeather("London").subscribe(data => {
       console.log("Weather data", data);
+      this.weatherData = data;
+    });
+  }
+
+
+  searchCountry(text: any): void {
+    this.weatherService.getCurrentWeather(text.target.value).subscribe(data => {
+      console.log("Search data", data);
       this.weatherData = data;
     });
   }
--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -9,8 +9,12 @@ export class WeatherService {
 
   constructor(private http: HttpClient){}
 
-  getCurrentWeather(): Observable<Object>{
-    return this.http.get<Object>("https://api.weatherapi.com/v1/current.json?key=f80570555e5344e5bb0191912240311&q=Kyiv");
+  getCurrentWeather(text: string): Observable<Object>{
+    return this.http.get<Object>(`https://api.weatherapi.com/v1/current.json?key=f80570555e5344e5bb0191912240311&q=${text}`);
+  }
+
+  searchWeather(searchTitle: string): Observable<any> {
+    return this.http.get<any>(`https://api.weatherapi.com/v1/search.json?key=f80570555e5344e5bb0191912240311&q=${searchTitle}`);
   }
 
 }
