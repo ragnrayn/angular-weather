8390a456609f9d1908f5091e4dd619430a22caac
ragnrayn <arthurkravits01@gmail.com>
2024-11-16 18:43:22 +0200

    added search-bar logic and timezone functionality

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -15,29 +15,48 @@ import { NgFor, NgIf } from '@angular/common';
 export class HomeComponent implements OnInit {
   weatherData: any = {};
   forecastWeather: any = [];
+  searchBarText: string = '';
+  cityTimezone: string = '';
+  errorHandler: Array<any> = [];
 
-  constructor(private weatherService: WeatherService) { }
+  constructor(private weatherService: WeatherService) {}
 
   ngOnInit(): void {
 
-    this.weatherService.forecastWeather("London", "5").subscribe(data => {
-      console.log("Forecast data", data);
+    this.weatherService.forecastWeather('London', '5').subscribe((data) => {
+      console.log('Forecast data', data);
       this.forecastWeather = data;
-    });
+    }
+  );
 
     this.weatherService.getCurrentWeather('London').subscribe((data) => {
       console.log('Weather data', data);
       this.weatherData = data;
     });
+
+    this.weatherService.getTimezone('London').subscribe((data) => {
+      console.log("Timezone", data);
+      this.cityTimezone = data.location.localtime;
+    })
   }
 
-
-  searchCountry(text: any): void {
+  searchCountry(): void {
     this.weatherService
-      .getCurrentWeather(text.target.value)
+      .getCurrentWeather(this.searchBarText)
       .subscribe((data) => {
         console.log('Search data', data);
         this.weatherData = data;
       });
+
+    this.weatherService.forecastWeather(this.searchBarText, '5').subscribe((data) => {
+      console.log('Forecast data', data);
+      this.forecastWeather = data;
+    });
+
+
+    this.weatherService.getTimezone(this.searchBarText).subscribe((data) => {
+      console.log("Timezone", data);
+      this.cityTimezone = data.location.localtime;
+    })
   }
 }
--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -11,8 +11,8 @@ export class WeatherService {
   
   constructor(private http: HttpClient){}
 
-  getCurrentWeather(text: string): Observable<Object>{
-    return this.http.get<Object>(`https://api.weatherapi.com/v1/current.json?key=f80570555e5344e5bb0191912240311&q=${text}`);
+  getCurrentWeather(city: string): Observable<Object>{
+    return this.http.get<Object>(`https://api.weatherapi.com/v1/current.json?key=f80570555e5344e5bb0191912240311&q=${city}`);
   }
 
   forecastWeather(city: string, days: string): Observable<any>{
@@ -22,5 +22,11 @@ export class WeatherService {
   searchWeather(searchTitle: string): Observable<any> {
     return this.http.get<any>(`https://api.weatherapi.com/v1/search.json?key=f80570555e5344e5bb0191912240311&q=${searchTitle}`);
   }
+
+
+  getTimezone(city: string): Observable<any> {
+    return this.http.get(`https://api.weatherapi.com/v1/timezone.json?key=f80570555e5344e5bb0191912240311&q=${city}`);
+  }
+
 
 }
